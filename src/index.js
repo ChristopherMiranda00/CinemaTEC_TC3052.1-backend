b742f23291ad9e34e22e35945fc6eb193c1ac58f
Luis Alcantara <emilioag@Luiss-MacBook-Pro-4.local>
2022-05-09 18:48:24 -0500

    src

---
+++ src/index.js
@@ -0,0 +1,42 @@
+require('./models/User');
+require('./models/List');
+require('./models/Movie');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/listRoutes');
+const movieRoutes = require('./routes/movieRoutes');
+const userRoutes = require('./routes/userRoutes');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(authRoutes);
+app.use(trackRoutes);
+app.use(movieRoutes);
+app.use(userRoutes);
+
+// MongoDB
+const mongoUri = 'mongodb+srv://admin:cinema@cinematec.4x0pf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+});
+mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB instance');
+});
+mongoose.connection.on('error', (err) => {
+    console.error('Error connecting to MongoDB instance', err)
+}); 
+
+app.get('/', (req, res) => {
+    res.send(`Welcome to the Flixer API!!!`);
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log('Listening on port 8080');
+});
